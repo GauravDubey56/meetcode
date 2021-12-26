56850f75e9e8c5fc589a967711b614c84de822e4
Gaurav Dubey <dubeygaurav0506@gmail.com>
2021-12-27 00:15:03 +0530

    should work fine on deployment

--- controllers/sendRequest.js
+++ controllers/sendRequest.js
@@ -3,7 +3,7 @@ const User = require("../models/user")
 const { auth, isLogedIn } = require('../Midlewares/auth.js');
 const dotenv = require("dotenv");
 var mongoose = require('mongoose');
-
+const fetch = require('node-fetch')
 dotenv.config({path: '../config/config.env'})
 // @desc : email the admin about a request to collab from the user
 // @route: /collab/join/:adminID 
@@ -15,25 +15,6 @@ exports.requestCollab = async (req, res, next) => {
      const adminID = await  req.params.id;
      const admin = await User.findById(mongoose.Types.ObjectId(adminID))
      console.log(adminID)
-     console.log(process.env.SENDGRID_API_KEY)
-     const sgMail = require('@sendgrid/mail');
-     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-          const msg = {
-          to: `${admin.username}`,
-          from: "qliodev@gmail.com",
-          subject: 'Request to collaborate',
-          body: 'I Want to contribute to the mentioned project. Lets connect',
-          html: `<strong>Hi, I am ${req.user.name}. I Want to contribute to the mentioned project. Lets connect. Ping me at @ ${contact}. </strong>`,
-          };
-     console.log(msg);
-     sgMail
-          .send(msg)
-          .then(() => {
-          console.log('Email sent')
-          })
-          .catch((error) => {
-          console.error(error)
-          })
-
+     fetch(`https://otp--flask-api.herokuapp.com/request?res=${admin.username}&sender=${contact}`)
      return res.redirect("/collab");
 }
